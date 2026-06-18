1026f7701242e3b2e361f6aedfcaceb872915e62
Chiranjibi Yadav <chiranjibiyadav6@gmail.com>
2026-06-18 08:43:26 +0530

    updated project files

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require("../models/owners-model");
 
 router.get("/", function(req,res){
+    res.send("hey its working");
+});
+
+// if(process.env === "development"){
+//     console.Console.log("hey");
+// }
+
+console.log(process.env.Node_ENV);
+
+router.post("/create", function(req,res){
     res.send("hey its working");
 });
 
